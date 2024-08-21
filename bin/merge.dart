14d8cd8a10b3b27307cc8ba8389455f66b058ebe
AbdulGhani9 <imabdulghani9@gmail.com>
2024-08-21 12:33:07 +0500

    first commit in master

---
+++ bin/merge.dart
@@ -0,0 +1,5 @@
+import 'package:merge/merge.dart' as merge;
+
+void main(List<String> arguments) {
+  print('Hello world: ${merge.calculate()}!');
+}
---
+++ bin/mergee.dart
@@ -0,0 +1,3 @@
+void main(){
+  print("master branch code ");
+}
